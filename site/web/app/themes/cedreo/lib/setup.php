9a6b7806843ccb80a691c384e5087a5c6128c968
wabdsgn <wab2com@gmail.com>
2016-03-09 18:43:20 +0100

    add modernizr

--- site/web/app/themes/cedreo/lib/setup.php
+++ site/web/app/themes/cedreo/lib/setup.php
@@ -101,6 +101,7 @@ function assets() {
     wp_enqueue_script('comment-reply');
   }
 
+  wp_enqueue_script('modernizr', Assets\asset_path('scripts/modernizr.js'), [], null, false);
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
