825873511830199d6059c979acf97a837f103ab4
wab <contact@wabdesign.fr>
2016-03-13 14:02:04 +0100

    menu grid

--- site/web/app/themes/cedreo/templates/header.php
+++ site/web/app/themes/cedreo/templates/header.php
@@ -1,23 +1,15 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>" title="retour à l'accueil"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/logo-brand.png" alt="<?php bloginfo('name'); ?>"></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-      <ul class="nav nav-inline">
-        <!-- <li class="nav-item"><a class="nav-link" href="#">languages</a></li> -->
-        <li class="nav-item">
-          <button class="hamburger hamburger--elastic toggle-nav" type="button">
-            <span class="hamburger-box">
-              <span class="hamburger-inner"></span>
-            </span>
-            <span class="txt text-uppercase">menu <small>(hit ESC)</small></span>
-          </button>
-        </li>
-      </ul>
-    </nav>
+  <div class="row align-middle">
+    <div class="columns">
+       <a class="brand" href="<?= esc_url(home_url('/')); ?>" title="retour à l'accueil"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/logo-brand.png" alt="<?php bloginfo('name'); ?>"></a>
+    </div>
+    <div class="columns">
+      <button class="hamburger hamburger--elastic toggle-nav" type="button">
+        <span class="hamburger-box">
+          <span class="hamburger-inner"></span>
+        </span>
+        <span class="txt text-uppercase">menu <small>(hit ESC)</small></span>
+      </button>
+    </div>
   </div>
 </header>
--- site/web/app/themes/cedreo/templates/menu.php
+++ site/web/app/themes/cedreo/templates/menu.php
@@ -1,9 +1,8 @@
 <nav class="main-navigation">
-	<div class="menu-wrap">
-		<div class="menu-col">
+	<ul class="row">
+		<li class="columns">
 			<h2 class="menu-title">Les secteurs d'activité</h2>
-			<hr>
-			<ul class="nav nav-menu">
+			<ul class="menu vertical">
 				<li class="nav-item"><a class="nav-link" href="#">Construction</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Mobilier / Aménagement maison</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Agencement d'espaces</a></li>
@@ -12,36 +11,39 @@
 				<li class="nav-item"><a class="nav-link" href="#">Cuisine &amp; bain</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Architecure 3D</a></li>
 			</ul>
-		</div>
-		<div class="menu-col">
+		</li>
+		<li class="columns">
 			<h2 class="menu-title">Nos solutions logiciels</h2>
-			<hr>
-			<ul class="nav nav-menu">
+			<ul class="menu vertical">
 				<li class="nav-item"><a class="nav-link" href="#">Configurateur 3D</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Logiciel d'agencement 3D</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Logiciel construction 3D</a></li>
 			</ul>
-		</div>
-		<div class="menu-col">
+		</li>
+		<li class="columns">
 			<h2 class="menu-title">Cedreo</h2>
-			<hr>
-			<ul class="nav nav-menu">
+			<ul class="menu vertical">
 				<li class="nav-item"><a class="nav-link" href="#">L'entreprise &amp; l'équipe</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Nos méthodes</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">RSE</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Rejoignez-nous</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">On parle de nous</a></li>
 			</ul>
-		</div>
-		<div class="menu-col full">
-			<hr>
-			<ul class="nav nav nav-inline">
+		</li>
+		<li class="columns small-12">
+			<ul class="menu">
 				<li class="nav-item"><a class="nav-link" href="#">L'actualité</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Nos réalisations</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Contactez-nous</a></li>
 				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-twitter"></i></a></li>
 				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-facebook"></i></a></li>
 			</ul>
-		</div>
-	</div>
+		</li>
+	</ul>
+	<?php
+    if (has_nav_menu('primary_navigation')) :
+      wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+    endif;
+  ?>
+
 </nav>
