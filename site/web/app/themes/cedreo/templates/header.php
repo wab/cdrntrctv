a0bb7a44e5e6a8da7cb62de07bb08e48278d1e06
wabdsgn <wab2com@gmail.com>
2016-03-14 15:17:24 +0100

    header, menu & main grid include

--- site/web/app/themes/cedreo/templates/header.php
+++ site/web/app/themes/cedreo/templates/header.php
@@ -1,5 +1,5 @@
 <header class="banner">
-  <div class="row align-middle">
+  <div class="row">
     <div class="columns">
        <a class="brand" href="<?= esc_url(home_url('/')); ?>" title="retour à l'accueil"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/logo-brand.png" alt="<?php bloginfo('name'); ?>"></a>
     </div>
@@ -8,7 +8,7 @@
         <span class="hamburger-box">
           <span class="hamburger-inner"></span>
         </span>
-        <span class="txt text-uppercase">menu <small>(hit ESC)</small></span>
+        <span class="txt text-uppercase">menu <small class="show-for-large">(press ESC)</small></span>
       </button>
     </div>
   </div>
--- site/web/app/themes/cedreo/templates/menu.php
+++ site/web/app/themes/cedreo/templates/menu.php
@@ -1,7 +1,7 @@
 <nav class="main-navigation">
 	<div class="row">
 		<div class="columns">
-			<h2 class="menu-title">Les secteurs d'activité</h2>
+			<h2 class="subheader">Les secteurs d'activité</h2>
 			<ul class="menu vertical">
 				<li class="nav-item"><a class="nav-link" href="#">Construction</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Mobilier / Aménagement maison</a></li>
@@ -11,17 +11,17 @@
 				<li class="nav-item"><a class="nav-link" href="#">Cuisine &amp; bain</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Architecure 3D</a></li>
 			</ul>
+			<div class="panel">
+				<h2 class="subheader">Nos solutions logiciels</h2>
+				<ul class="menu vertical">
+					<li class="nav-item"><a class="nav-link" href="#">Configurateur 3D</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Logiciel d'agencement 3D</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Logiciel construction 3D</a></li>
+				</ul>
+			</div>
 		</div>
 		<div class="columns">
-			<h2 class="menu-title">Nos solutions logiciels</h2>
-			<ul class="menu vertical">
-				<li class="nav-item"><a class="nav-link" href="#">Configurateur 3D</a></li>
-				<li class="nav-item"><a class="nav-link" href="#">Logiciel d'agencement 3D</a></li>
-				<li class="nav-item"><a class="nav-link" href="#">Logiciel construction 3D</a></li>
-			</ul>
-		</div>
-		<div class="columns">
-			<h2 class="menu-title">Cedreo</h2>
+			<h2 class="subheader">Cedreo</h2>
 			<ul class="menu vertical">
 				<li class="nav-item"><a class="nav-link" href="#">L'entreprise &amp; l'équipe</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Nos méthodes</a></li>
@@ -29,12 +29,14 @@
 				<li class="nav-item"><a class="nav-link" href="#">Rejoignez-nous</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">On parle de nous</a></li>
 			</ul>
-		</div>
-		<div class="columns small-12">
-			<ul class="menu">
-				<li class="nav-item"><a class="nav-link" href="#">L'actualité</a></li>
-				<li class="nav-item"><a class="nav-link" href="#">Nos réalisations</a></li>
-				<li class="nav-item"><a class="nav-link" href="#">Contactez-nous</a></li>
+			<hr>
+			<ul class="menu vertical">
+				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-newspaper-o"></i> L'actualité</a></li>
+				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-picture-o"></i> Nos réalisations</a></li>
+				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-envelope"></i> Contactez-nous</a></li>
+			</ul>
+			<hr>
+			<ul class="menu reseaux">
 				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-twitter"></i></a></li>
 				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-facebook"></i></a></li>
 			</ul>
