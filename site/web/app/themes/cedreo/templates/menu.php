d173184b6f0a7e6230e5a9726468868743846add
wabdsgn <wab2com@gmail.com>
2016-03-22 13:29:31 +0100

    responsive menu

--- site/web/app/themes/cedreo/templates/menu.php
+++ site/web/app/themes/cedreo/templates/menu.php
@@ -1,5 +1,5 @@
 <nav class="main-navigation">
-	<div class="row">
+	<div class="row show-for-medium">
 		<div class="columns">
 			<h2 class="subheader">Les secteurs d'activité</h2>
 			<ul class="menu vertical">
@@ -19,7 +19,7 @@
 					<li class="nav-item"><a class="nav-link" href="#">Logiciel construction 3D</a></li>
 				</ul>
 			</div>
-		</div>
+		</div> <!-- columns -->
 		<div class="columns">
 			<h2 class="subheader">Cedreo</h2>
 			<ul class="menu vertical">
@@ -30,6 +30,7 @@
 				<li class="nav-item"><a class="nav-link" href="#">On parle de nous</a></li>
 			</ul>
 			<hr>
+			<h2 class="subheader">Nous suivre</h2>
 			<ul class="menu vertical">
 				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-newspaper-o"></i> L'actualité</a></li>
 				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-picture-o"></i> Nos réalisations</a></li>
@@ -40,12 +41,53 @@
 				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-twitter"></i></a></li>
 				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-facebook"></i></a></li>
 			</ul>
-		</div>
-	</ul>
-	<?php
-    if (has_nav_menu('primary_navigation')) :
-      wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-    endif;
-  ?>
-
+		</div> <!-- columns -->
+	</div> <!-- row -->
+	
+	<div class="row columns hide-for-medium ">
+		<ul class="vertical menu" data-drilldown>
+		  <li>
+		    <a href="#">Les secteurs d'activité</a>
+		    <ul class="menu vertical">
+				<li class="nav-item"><a class="nav-link" href="#">Construction</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Mobilier / Aménagement maison</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Agencement d'espaces</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Immobilier</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Merchandasing</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Cuisine &amp; bain</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Architecure 3D</a></li>
+			</ul>
+		  </li>
+		  <li>
+		    <a href="#">Nos solutions logiciels</a>
+		    <ul class="menu vertical">
+				<li class="nav-item"><a class="nav-link" href="#">Configurateur 3D</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Logiciel d'agencement 3D</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Logiciel construction 3D</a></li>
+			</ul>
+		  </li>
+		  <li>
+		    <a href="#">Cedreo</a>
+		    <ul class="menu vertical">
+				<li class="nav-item"><a class="nav-link" href="#">Construction</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Mobilier / Aménagement maison</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Agencement d'espaces</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Immobilier</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Merchandasing</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Cuisine &amp; bain</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Architecure 3D</a></li>
+			</ul>
+		  </li>
+		  <li>
+		    <a href="#">Nous suivre</a>
+		    <ul class="menu vertical">
+				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-newspaper-o"></i> L'actualité</a></li>
+				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-picture-o"></i> Nos réalisations</a></li>
+				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-envelope"></i> Contactez-nous</a></li>
+				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-twitter"></i> Twitter</a></li>
+				<li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-facebook"></i> Facebook</a></li>
+			</ul>
+		  </li>
+		</ul>
+	</div>
 </nav>
