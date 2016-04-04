066a7a867b70b3569edf11da6e635210b5b773fa
wabdsgn <wab2com@gmail.com>
2016-04-04 16:25:29 +0200

    entreprise & contact template carousel z-index

---
+++ site/web/app/themes/cedreo/page-contact.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Page contact
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+	<?php get_template_part('templates/page', 'header'); ?>
+	<div class="section contenu">
+		<div class="row">
+			<div class="columns large-8">
+				<?php if (get_field('subtitle')) : ?>
+					<h2 class="cedreo-title subtitle"><?php the_field('subtitle'); ?></h2>
+				<?php endif; ?>
+				<?php get_template_part('templates/content', 'page'); ?>
+			</div>
+		</div>
+	</div>
+
+<?php endwhile; ?>
--- site/web/app/themes/cedreo/page-entreprise.php
+++ site/web/app/themes/cedreo/page-entreprise.php
@@ -1,3 +1,10 @@
+<?php
+
+/**
+ * Template Name: Page entreprise
+ */
+
+?>
 <?php while (have_posts()) : the_post(); ?>
 	<?php get_template_part('templates/page', 'header'); ?>
 	<div class="section contenu">
