0bbdbff8929fafa1761bd0e0c6f6d22214762b13
wabdsgn <wab2com@gmail.com>
2016-04-05 18:04:33 +0200

    sidebar

--- site/web/app/themes/cedreo/base.php
+++ site/web/app/themes/cedreo/base.php
@@ -28,7 +28,7 @@ use Roots\Sage\Breadcrumbs;
 
            
           <div class="banshadow">
-            <header class="page-header section" <?php if (has_post_thumbnail()) { ?>style="background-image: url(<?php the_post_thumbnail_url('banner'); ?>);"<?php } ?>>
+            <header class="page-header section">
 
               <?php if( !is_singular('post') ) : ?>
                 <h1 class="page-title cedreo-title">l'Actualité <span>Cedreo</span></h1>
--- site/web/app/themes/cedreo/templates/page-header.php
+++ site/web/app/themes/cedreo/templates/page-header.php
@@ -3,7 +3,7 @@
 	use Roots\Sage\Breadcrumbs;
 ?>
 <div class="banshadow">
-	<header class="page-header section" <?php if (has_post_thumbnail()) { ?>style="background-image: url(<?php the_post_thumbnail_url('banner'); ?>);"<?php } ?>>
+	<header class="page-header section" <?php if (has_post_thumbnail() ) { ?>style="background-image: url(<?php the_post_thumbnail_url('banner'); ?>);"<?php } ?>>
 
   		<h1 class="page-title cedreo-title"><?= Titles\title(); ?></h1>
   		<?= Breadcrumbs\breadcrumbs(); ?>
