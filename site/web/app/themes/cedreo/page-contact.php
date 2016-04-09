135f6405e897068319e96267deadd8867bbd076b
wab <contact@wabdesign.fr>
2016-04-09 23:49:10 +0200

    page contact update plugins & database

--- site/web/app/themes/cedreo/page-contact.php
+++ site/web/app/themes/cedreo/page-contact.php
@@ -8,18 +8,44 @@
 	<?php get_template_part('templates/page', 'header'); ?>
 	<div class="section contenu">
 		<div class="row">
-			<div class="columns">
-				<div id="map"></div>
-			</div>
+
+			<?php $location = get_field('geoloc');
+
+				if( !empty($location) ): ?>
+
+				<div class="columns">
+					<div id="map"></div>
+				</div>
+			<?php endif; ?>
 
 			<div class="columns large-4">
+				<?php 
+
+				$image = get_field('image');
+				$size = 'square';
+				$thumb = $image['sizes'][ $size ];
+
+				if( !empty($image) ): ?>
+
+					<p><img src="<?php echo $thumb; ?>" alt="<?php echo $image['alt']; ?>" class="img-rounded" /></p>
+
+				<?php endif; ?>
+				
+				<?php if ( get_field('adresse') || get_field('telephone') ) : ?>
+				<ul class="no-bullet list-contact">
+					<?php if ( get_field('adresse') ) { echo '<li class="item address"><address>' . get_field('adresse') . '</address></li>'; } ?>
+					<?php if ( get_field('telephone') ) { echo '<li class="item tel">' . get_field('telephone') . '</li>'; } ?>
+				</ul>
+				<?php endif; ?>
+
+			</div>
+
+			<div class="columns large-8">
 				<?php if (get_field('subtitle')) : ?>
 					<h2 class="cedreo-title subtitle"><?php the_field('subtitle'); ?></h2>
 				<?php endif; ?>
 				<?php get_template_part('templates/content', 'page'); ?>
-			</div>
-
-			<div class="columns large-8">
+				<p class="lead"><i class="fa fa-envelope"></i> Contactez-nous</p>
 				<?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 1 ); } ?>
 			</div>
 		</div>
@@ -27,9 +53,13 @@
 
 	<script type="text/javascript">
 
-		var map;
+		var map, lat, lng;
+		
+		lat = <?php echo $location['lat']; ?>;
+		lng = <?php echo $location['lng']; ?>;
+		
 		function initMap() {
-			var LatLng = {lat: 47.2392585, lng: -1.7303783};
+			var LatLng = {lat: lat, lng: lng};
 			var image = '<?php echo get_template_directory_uri(); ?>/dist/images/marker.png';
 			
 			map = new google.maps.Map(document.getElementById('map'), {
