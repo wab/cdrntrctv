a71c3dd0028ded87136b8e064b7a96ce85cf7e4c
wab <wab2com@gmail.com>
2016-05-17 16:09:12 +0200

    page-section int.

--- site/web/app/themes/cedreo/templates/section.php
+++ site/web/app/themes/cedreo/templates/section.php
@@ -3,21 +3,58 @@
 // check if the repeater field has rows of data
 if( have_rows('section') ): ?>
 
-<section class="section">
-	<div class="row columns">
+
 
 	<?php 	// loop through the rows of data
     while ( have_rows('section') ) : the_row(); ?>
 
-        <h2 class="cedreo-title subtitle"><?php the_sub_field('title'); ?></h2>
+    <section class="section page-section">
+		<div class="row column">
 
-        <div class="media"><?php the_sub_field('media-video'); ?></div>
-        <div class="contenu"><?php the_sub_field('contenu'); ?></div>
+	        <h2 class="cedreo-title subtitle large-8 large-centered columns"><?php the_sub_field('title'); ?></h2>
+
+	        <div class="media medium-6 columns">
+
+	        	<?php if (get_sub_field('media') == 'image') : ?>
+
+	        		<?php 
+	        			$image = get_sub_field('media-img');
+	        			// thumbnail
+						$size = 'square';
+						$thumb = $image['sizes'][ $size ];
+	        		?>
+
+	        		<?php if( !empty($image) ): ?>
+
+						<img src="<?php echo $thumb; ?>" alt="<?php echo $image['alt']; ?>" />
+
+					<?php endif; ?>
+
+	        	<?php else : ?>
+					
+					<div class="flex-video widescreen">
+						<iframe type="text/html" src="https://www.youtube.com/embed/<?php the_sub_field('media-video'); ?>?controls=0&color=white&theme=light" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+	        	<?php endif ?>
+	        		
+	        	</div>
+
+	        <div class="contenu medium-6 columns">
+	        	<?php the_sub_field('contenu'); ?>
+	        	<?php if ( get_sub_field('action-link')) : ?>
+	        		<p><a href="<?php the_sub_field('action-link'); ?>" class="button"><?php the_sub_field('action-title'); ?></a></p>
+	        	<?php endif ?>
+	        	
+	        </div>
+
+		</div>
+    </section>
 
     <?php endwhile; ?>
 
-	</div>
+	
 
-</section>
+
 
 <?php endif; ?>
