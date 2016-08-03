bc5f5ef6fbe595444746f92a7115e0807b6edde9
wab <contact@wabdesign.fr>
2016-08-03 21:43:09 +0200

    maj & g meta verif

--- site/web/app/themes/cedreo/templates/head.php
+++ site/web/app/themes/cedreo/templates/head.php
@@ -2,8 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<?php get_template_part('templates/favicons'); ?>  
+  <meta name="google-site-verification" content="gxSY-onb8Gw5CP5RB9l2qo7sLzRAl6z_E0N4A5hnifQ" />
+
+	<?php get_template_part('templates/favicons'); ?>
 
   <?php wp_head(); ?>
 </head>
